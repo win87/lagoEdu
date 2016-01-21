eb054d13b211216a00461d295fc7eda9a56bce77
jordonzeng <zengwei822@gmail.com>
2016-01-20 22:04:42 -0800

    New school info

--- school-info.php
+++ school-info.php
@@ -7,14 +7,6 @@
 
         <?php require 'breadcrumbs.php' ?>
 
-        <!-- Image Header -->
-        <div class="row">
-            <div class="col-lg-12">
-                <img class="img-responsive school-home-img" src="<?php echo $imgUrl; ?>" alt="">
-            </div>
-        </div>
-        <!-- /.row -->
-
         <!-- Portfolio Item Row -->
         <div class="row">
 
@@ -52,13 +44,26 @@
 
             <div class="col-md-4">
                 <h3>Project Description</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
-                <h3>Project Details</h3>
                 <ul>
                     <li>Lorem Ipsum</li>
                     <li>Dolor Sit Amet</li>
                     <li>Consectetur</li>
                     <li>Adipiscing Elit</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Dolor Sit Amet</li>
+                    <li>Consectetur</li>
+                    <li>Adipiscing Elit</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Dolor Sit Amet</li>
+                    <li>Consectetur</li>
+                    <li>Adipiscing Elit</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Dolor Sit Amet</li>
+                    <li>Consectetur</li>
+                    <li>Adipiscing Elit</li>
+                    <li>Consectetur</li>
+                    <li>Adipiscing Elit</li>
+
                 </ul>
             </div>
 
